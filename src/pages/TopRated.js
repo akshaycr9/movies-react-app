3b81f58b92cb1257b5e4db200c5c0d86cb4743ab
Akshay Thadani <akshay.thadani@gmail.com>
2022-03-19 15:20:42 +0530

    Added infinite scroll for all tabs

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MoviesList from '../components/MoviesList';
+import useInfiniteScroll from '../hooks/useInfiniteScroll';
 
 const TopRated = () => {
-  return <div>Top Rated</div>;
+  const [movies, setMovies] = useState([]);
+  const [isFetching, setIsFetching] = useInfiniteScroll(getMovies);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  async function getMovies(pageNo = 1) {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?page=${pageNo}&api_key=0e7274f05c36db12cbe71d9ab0393d47`
+    );
+    const data = await response.json();
+    setMovies((prevState) => [...prevState, ...data.results]);
+    setIsFetching(false);
+  }
+
+  return <MoviesList movies={movies} isFetching={isFetching} />;
 };
 
 export default TopRated;
