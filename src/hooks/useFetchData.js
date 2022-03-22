44a8956ae9b5522c75785766c574a6707319a645
Akshay Thadani <akshay.thadani@gmail.com>
2022-03-22 11:32:22 +0530

    Refactored search page

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,19 +1,36 @@
 import { useEffect, useState } from 'react';
 import useInfiniteScroll from './useInfiniteScroll';
 
-const useFetchData = (pageType) => {
+const useFetchData = (pageType, query) => {
   const BASE_URL = 'https://api.themoviedb.org/3/movie';
+  const SEARCH_BASE_URL = 'https://api.themoviedb.org/3/search/movie';
   const API_KEY = '0e7274f05c36db12cbe71d9ab0393d47'; //Should be present in env variable but for simplicity I am hardcoding it here
   const [movies, setMovies] = useState([]);
-  const [isFetching, setIsFetching] = useInfiniteScroll(getMovies);
+  const [isFetching, setIsFetching] = useInfiniteScroll(
+    pageType === 'search' ? searchMovie : getMovies
+  );
 
   useEffect(() => {
-    getMovies();
-  }, []);
+    if (pageType === 'search') {
+      setMovies([]);
+      searchMovie();
+    } else {
+      getMovies();
+    }
+  }, [query]);
 
   async function getMovies(pageNo = 1) {
     const response = await fetch(
       `${BASE_URL}/${pageType}?page=${pageNo}&api_key=${API_KEY}`
+    );
+    const data = await response.json();
+    setMovies((prevState) => [...prevState, ...data.results]);
+    setIsFetching(false);
+  }
+
+  async function searchMovie(pageNo = 1) {
+    const response = await fetch(
+      `${SEARCH_BASE_URL}?query=${query}&page=${pageNo}&api_key=${API_KEY}`
     );
     const data = await response.json();
     setMovies((prevState) => [...prevState, ...data.results]);
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,30 +1,14 @@
-import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import useInfiniteScroll from '../hooks/useInfiniteScroll';
 import MoviesList from '../components/MoviesList';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Loader from '../components/Loader';
+import useFetchData from '../hooks/useFetchData';
 
 const Search = ({ genres }) => {
   let { query } = useParams();
 
-  const [movies, setMovies] = useState([]);
-  const [isFetching, setIsFetching] = useInfiniteScroll(searchMovie);
-
-  useEffect(() => {
-    setMovies([]);
-    searchMovie();
-  }, [query]);
-
-  async function searchMovie(pageNo = 1) {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${query}&page=${pageNo}&api_key=0e7274f05c36db12cbe71d9ab0393d47`
-    );
-    const data = await response.json();
-    setMovies((prevState) => [...prevState, ...data.results]);
-    setIsFetching(false);
-  }
+  const [movies, isFetching] = useFetchData('search', query);
 
   return (
     <Container maxWidth="xl">
