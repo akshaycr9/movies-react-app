c2cca4900c50c21137c69ae58151f5b95770a2ef
Akshay Thadani <akshay.thadani@gmail.com>
2022-03-22 12:05:10 +0530

    Added background image for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Paper } from '@mui/material';
+import HomeBackground from '../images/home-background.JPG';
 
 const Home = () => {
-  return <div>Home</div>;
+  const styles = {
+    paperContainer: {
+      backgroundImage: `url(${HomeBackground})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      height: '100vh',
+      width: '100vw',
+    },
+  };
+
+  return <Paper style={styles.paperContainer}></Paper>;
 };
 
 export default Home;
